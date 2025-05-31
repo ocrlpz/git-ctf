c8622775cead325dab23fd3a08fbdea5a89a6a22
ocrlpz <hrswakym@gmail.com>
2025-05-30 20:49:50 -0600

    Funcionalidad secreta integrada: muestra FLAG correctamente

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const txt = document.querySelector('#text-flag');
 
 const actionBtn = ()=>{
     //Flag para ganar un punto en el parcial
-    txt.innerHTML = 'Aquí no hay flag'
+    txt.innerHTML = 'FLAG{gatos_dominaran_el_mundo}';
 }
 
 btn.addEventListener('click' , actionBtn);
